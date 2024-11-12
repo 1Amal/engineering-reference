ca6b56143c74af94ea69a3ef1176e92e2914c8b1
Amal Kariyawasam <12756149+1Amal@users.noreply.github.com>
2024-11-12 21:27:54 +1100

    UChanged Folder Structure

---
+++ DevOps-&-Developer-Tools/Jest/style-mock.js
@@ -0,0 +1,2 @@
+//This file is used as a Mock for Jest tests for CSS files, without this it will give an error trying to parse CSS files in Jest tests
+module.exports = {};
---
+++ DevOps-&-Developer-Tools/Linux/Linux-Scripts/ubuntuFullUpdateWithoutPrompts.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+sudo apt update -y && sudo apt full-upgrade -y && sudo apt autoremove -y && sudo apt clean -y && sudo apt autoclean -y
